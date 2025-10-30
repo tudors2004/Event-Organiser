34d2884f725039444d5aab69fcbf1d562eb47675
Tudor Sarghiuta <149654961+tudors2004@users.noreply.github.com>
2025-10-30 19:13:03 +0200

    Initial commit

---
+++ src/main/java/org/example/eventorganiser/EventOrganiserApplication.java
@@ -0,0 +1,13 @@
+package org.example.eventorganiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventOrganiserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventOrganiserApplication.class, args);
+    }
+
+}
