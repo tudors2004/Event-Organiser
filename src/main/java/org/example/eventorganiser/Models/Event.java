c6e5c437c84270aed09f3c6a22be65ba7af4db78
Raul1963 <raulmarginean1963@gmail.com>
2025-11-05 21:57:16 +0200

    created tables in postgres and added missing list to event and user

--- src/main/java/org/example/eventorganiser/Models/Event.java
+++ src/main/java/org/example/eventorganiser/Models/Event.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 
 @Entity
+@Table(name="events")
 public class Event {
     @Id
     @GeneratedValue
@@ -20,4 +21,63 @@ public class Event {
 
     @OneToMany(mappedBy = "event", cascade = CascadeType.ALL)
     private List<EventGuests> event_guests;
+
+    @ManyToMany(mappedBy = "organisedEvents")
+    private List<User> organizers;
+
+    public Event() {}
+
+    public Event(String eventName, Date eventDate, String event_location) {
+        this.eventName = eventName;
+        this.eventDate = eventDate;
+        this.event_location = event_location;
+    }
+
+    public int getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(int eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public Date getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(Date eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public String getEvent_location() {
+        return event_location;
+    }
+
+    public void setEvent_location(String event_location) {
+        this.event_location = event_location;
+    }
+
+    public List<EventGuests> getEvent_guests() {
+        return event_guests;
+    }
+
+    public void setEvent_guests(List<EventGuests> event_guests) {
+        this.event_guests = event_guests;
+    }
+
+    public List<User> getOrganizers() {
+        return organizers;
+    }
+
+    public void setOrganizers(List<User> organizers) {
+        this.organizers = organizers;
+    }
 }
--- src/main/java/org/example/eventorganiser/Models/User.java
+++ src/main/java/org/example/eventorganiser/Models/User.java
@@ -2,10 +2,12 @@ package org.example.eventorganiser.Models;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 @Entity
+@Table(name="users")
 public class User {
 
     @Id
@@ -14,12 +16,21 @@ public class User {
 
     private String name;
 
+    @Column(unique = true)
     private String email;
 
     private String password;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private List<EventGuests> events;
+
+    @ManyToMany
+    @JoinTable(
+            name = "event_organisers",
+            joinColumns = @JoinColumn(name = "organiser_id"),
+            inverseJoinColumns = @JoinColumn(name = "event_id")
+    )
+    private List<Event> organisedEvents;
     public User() {}
 
     public User(int userId, String name, String email, String password) {}
