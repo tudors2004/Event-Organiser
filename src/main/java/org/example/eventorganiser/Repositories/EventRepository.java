f7ade9715f41bddd0dd63d34e47c9581f186ec76
Vaidos Mihai <149139408+mihaivaidos@users.noreply.github.com>
2025-11-23 18:29:01 +0200

    Feat see all events (#5)
    * Add findAllEventsByUserId method in EventRepository
    
    * Add getAllEventsForUser method in EventService
    
    * Add getAllEventsForUser method in EventController

--- src/main/java/org/example/eventorganiser/Repositories/EventRepository.java
+++ src/main/java/org/example/eventorganiser/Repositories/EventRepository.java
@@ -1,11 +1,17 @@
 package org.example.eventorganiser.Repositories;
 
 import org.example.eventorganiser.Models.Event;
-import org.example.eventorganiser.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface EventRepository extends JpaRepository<Event, Long> {}
+public interface EventRepository extends JpaRepository<Event, Long> {
+
+    @Query("SELECT DISTINCT e FROM Event e LEFT JOIN e.organizers o LEFT JOIN e.eventGuests eg " +
+           "WHERE o.userId = :userId OR eg.user.userId = :userId")
+    List<Event> findAllEventsByUserId(@Param("userId") Integer userId);
+}
